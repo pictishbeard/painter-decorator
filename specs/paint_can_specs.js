614599b2ed1e7b8f66e864a74adec82e7b478df4
sailorj94 <jackniall9@gmail.com>
2021-05-04 15:41:03 +0100

    Should have paint test written

---
+++ specs/paint_can_specs.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const Paint_Can = require('../paint_can.js');
+
+let paint_can;
+
+describe('Paint Can', function () {
+    beforeEach(function () {
+        paint_can = new Paint_Can(12);
+    });
+
+    it('should have paint in can', function() {
+        const actual = paint_can.paint_volume();
+        assert.strictEqual(actual, 12);
+    })
+})
